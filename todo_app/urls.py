fa1fc02d064242c0dfa06d19da173e92886bba4e
syashakash <akashshivram9@gmail.com>
2020-03-07 22:43:51 +0800

    Add debug_toolbar to urlpattern to include in dev env

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls import include, url
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
