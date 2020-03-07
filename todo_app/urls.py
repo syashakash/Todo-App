2f3d944dfa939dba7804f15c1caad23efb5c7695
syashakash <akashshivram9@gmail.com>
2020-03-07 23:47:28 +0800

    Add favicon to url and update index.html to show vue front page

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -18,11 +18,31 @@ from django.urls import path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls import include, url
+from django.views.static import serve
+import os
+from django.views.generic.base import RedirectView
+favicon_view = RedirectView.as_view(url=os.path.join(settings.STATIC_URL,'favicon.ico'), permanent=True)
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
+    url(r'^dmedia/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(settings.VUE_ROOT, 'media')}),
+    url(r'^img/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(settings.VUE_ROOT, 'img')}),
+    url(r'^js/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(settings.VUE_ROOT, 'js')}),
+    url(r'^css/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(settings.VUE_ROOT, 'css')}),
+    url(r'^fonts/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(settings.VUE_ROOT, 'fonts')}),
+    path('favicon.ico', favicon_view),
 ]
 
 if settings.DEBUG:
