943844833bff7307669b7864bb87a923ae8a76af
syashakash <akashshivram9@gmail.com>
2020-03-07 22:03:41 +0800

    Add run script for envs

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ENV/bin/activate
+
+case $1 in
+  dev)
+    python manage.py runserver --settings=todo_app.settings.dev
+    ;;
+  prod)
+  	python manage.py collectstatic --noinput
+    python manage.py makemigrations    
+    python manage.py migrate
+    python manage.py runserver --settings=todo_app.settings.prod
+    ;;
+esac
